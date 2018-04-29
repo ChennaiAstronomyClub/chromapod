a95e0596069b13556c675448ef763719d904a098
Sivasubramanyam A <ssdragonar5@gmail.com>
2018-04-29 15:42:39 +0530

    Support HTML APODs, ES6ify codebase

--- source/script.js
+++ source/script.js
@@ -1,82 +1,63 @@
-$(document).ready(function() {
+const handleApod = (apod) => {
+    const { title, explanation: description } = apod;
+    let { url } = apod;
+    const copyright = apod.copyright ? `<br>Copyright: ${apod.copyright}` : '';
+    
+    const isVideo = url.includes('youtube') || url.includes('vimeo');
+    const isHtml = url.endsWith('.html');
+  
+    if (!isVideo && !isHtml) {
+      $('#apod').attr('src', url);
+    } else {
+      if (!url.startsWith('https')) {
+        url = `https://${url}`;
+      }
 
-    var API = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',
-        date = new Date(),
-        delayedDate = new Date(date.getTime() - 10*60000),
-        dateString = delayedDate.toLocaleString('en-US', { timeZone: 'America/New_York' }),
-        todaysDate = dateString.split(',')[0]
-        apodDate = localStorage.getItem('apodDate');
-
-    // Use the API response in localStorage if the API response has already been obtained.
-    if (apodDate == todaysDate) {
-
-        var apod = JSON.parse(localStorage.getItem('apod')),
-            title = apod['title'],
-            description = apod['explanation'],
-            url = apod['url'],
-            copyright = ((apod['copyright'] === undefined) ? '' : '<br>Copyright: ' + apod['copyright']); 
-
-        if (url.indexOf('youtube') == -1) {
-            $("#apod").attr("src", url);
-        }
-        else {
-            $("img").replaceWith('<iframe id="apod" src="' + 
-                url + '" style="border: 0; top:0; left:0; right:0; bottom:0; width:100%; height:100%;position: fixed;"></iframe>');
-        }
-        
-        $('#title').html(title);
-        $('#description').html(description + copyright); 
-
+      $('img').replaceWith(
+        `<iframe id="apod" src="${url}" class="apod-iframe"></iframe>`
+      );
     }
-
-    else {
-
-        $.ajax({
-
-            url : API,
-            dataType: 'json',
-            success: function(response) {
-
-                apod = response;
-                var title = apod['title'],
-                    description = apod['explanation'],
-                    url = apod['url'],
-                    copyright = ((apod['copyright'] === undefined) ? '' : '<br>Copyright: ' + apod['copyright']); 
-
-                // Save API response in localStorage along with date to avoid API calls on every new tab.
-                localStorage.setItem('apod', JSON.stringify(apod));
-                localStorage.setItem('apodDate', todaysDate);
-
-                if (url.indexOf('youtube') == -1) {
-                    $("#apod").attr("src", url);
-                }
-                else {
-                    $("img").replaceWith('<iframe id="apod" src="' + 
-                        url + '" style="border: 0; top:0; left:0; right:0; bottom:0; width:100%; height:100%;position: fixed;"></iframe>');
-                }
-                
-                $('#title').html(title);
-                $('#description').html(description + copyright);
-
-            },
-            error: function() {
-
-                $("#apod").attr("src", "fallback.jpg");
-                $('#title').html("Star Trails");
-                $('#description').html("APOD is unreachable. But CAC isn't. Here is a star trail taken at Nagalapuram by Sidharth Srinivasan.");
-            
-            }
-
-        });
-
-    }
+  
+    $('#title').html(title);
+    $('#description').html(`${description}${copyright}`); 
 
     $('#info-icon').hover(function() {
-        document.getElementById('info-dialog').showModal();
+      document.getElementById('info-dialog').showModal();
     }); 
 
     $('#info-dialog').click(function() {
-        document.getElementById('info-dialog').close();
+      document.getElementById('info-dialog').close();
     }); 
-
-});
+  };
+  
+  $(document).ready(() => {
+    const API = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
+    const delayedDate = new Date((new Date()).getTime() - 10*60000);
+    const dateString = delayedDate.toLocaleString('en-US', { timeZone: 'America/New_York' });
+    const [todaysDate] = dateString.split(',');
+    const apodDate = localStorage.getItem('apodDate');
+  
+    // Use the API response in localStorage if the API response has already been obtained.
+    if (apodDate == todaysDate) {
+      const apod = JSON.parse(localStorage.getItem('apod'));
+  
+      handleApod(apod);
+    } else {
+      $.ajax({
+        url : API,
+        dataType: 'json',
+        success: function(response) {
+          localStorage.setItem('apod', JSON.stringify(response));
+          localStorage.setItem('apodDate', todaysDate);
+  
+          handleApod(response);
+        },
+        error: function() {
+          $('#apod').attr('src', 'fallback.jpg');
+          $('#title').html('Star Trails');
+          $('#description').html('APOD is unreachable. But CAC isn\'t. Here is a star trail taken at Nagalapuram by Sidharth Srinivasan.');
+        }
+      });
+    }
+  });
+  
